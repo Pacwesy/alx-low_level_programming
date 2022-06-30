0762def1b9a40bb07f33fc5248449dd475f6942b
Pacwesy <Pacwesy@github.com>
2022-06-30 10:09:21 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #!/bin/bash
 #include <stdio.h>
-int main(void) {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+/**
+ *Description: main - Display a string to stdout.
+ *Return: 0 if success.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
