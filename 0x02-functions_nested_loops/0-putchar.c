17aa75d42b966b8e002a1191b177b08bae4602ec
Pacwesy <Pacwesy@github.com>
2022-07-05 12:22:12 -0700

    puchar fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
-#include "_putchar"
+
 /**
- * main -  program that prints _putchar.
+ * main - program that prints _putchar.
  *
  * Return: 0 always
  */
 int main(void)
 {
-int n = 0;
-char str_hbt[9] = "_putchar";
-while (n < 9)
-{
-_putchar(str_hbt[n]);
-n += 1;
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
