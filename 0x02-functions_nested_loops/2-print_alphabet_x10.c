ab352c6717810be6a2a4b41f6c5de25be77bd232
Pacwesy <Pacwesy@github.com>
2022-07-05 13:05:07 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print out -> alphabets in lowercase
+*/
+void print alphabet(void)
+{
+int s;
+int y;
+for (s = 0; s < 10; s++)
+{
+for (y = 'a'; y <= 'p'; y++)
+{
+_putchar(s);
+_putchar('\n');
+}
+}
+}
