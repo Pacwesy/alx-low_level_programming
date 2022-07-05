a946af178e714afc285d28de153534d75c6599e8
Pacwesy <Pacwesy@github.com>
2022-07-05 15:25:19 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs -function that computes the absolute value of an integer.
+ * @p: The input number to check
+ * Return: int.
+ */
+int int _abs(int p)
+{
+if (p < 0)
+{
+p = p * (-1);
+return (p);
+}
+else
+return (p);
+}
