e4494f0b60a0bb245c9a52d63f91369188b0ce53
Pacwesy <Pacwesy@github.com>
2022-07-05 14:59:35 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_sign -> prints sign based on condition.
+ *
+ *@n: checks for input number
+ *return: int.
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(48);
+return (0);
+}
+}
