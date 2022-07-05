307dcacc11714270a99e1252fec10722454ecf27
Pacwesy <Pacwesy@github.com>
2022-07-05 09:44:22 -0700

    hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,15 @@
  */
 int main(void)
 {
-int n = 0;
-char str_hbt[9] = "main";
-while (n < 9)
-{
-_putchar(str_hbt[n]);
-n += 1;
-}
-_putchar('\n');
+_putchar(' _ ');
+_putchar(' p ');
+_putchar(' u ');
+_putchar(' t ');
+_putchar(' c ');
+_putchar(' h ');
+_putchar(' a ');
+_putchar(' r ');
+_putchar(' \n ');
 return (0);
+
 }
